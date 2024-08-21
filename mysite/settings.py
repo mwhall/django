d723eb9ac40787c2c5e0434705d4c5b8102ff956
Michael Hall <hallm2533@gmail.com>
2024-08-21 11:40:26 -0300

    Tweaks to get media files working

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["https://django-server-production-af66.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = [["https://*.up.railway.app"]
 
 # Application definition
 
@@ -126,10 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(os.environ["RAILWAY_VOLUME_MOUNT_PATH"],"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
